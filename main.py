543741228871132abce78629e3c073e8e8736e29
Dima <orlyk.do@gmail.com>
2025-11-27 14:38:33 +0100

    Додано маршрут для виведення рандомного факту зі списку

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from routers.calculator import router as calc_router
 from routers import formulas
 from routers.games import router as games_router
 from routers.conversion import router as conv_router
+from routers.math_facts import router as facts_router
 
 Base.metadata.create_all(bind=engine)
 
@@ -15,6 +16,7 @@ app = FastAPI()
 
 app.include_router(auth_router, prefix="/auth", tags=["Авторизація"])
 app.include_router(calc_router, prefix="/calculator", tags=["Калькулятор"])
-app.include_router(formulas.router, prefix="/formulas", tags=["Формули фігур на площині і в об'ємі"])
-app.include_router(games_router, prefix="/games", tags=["Міні-ігри"])
 app.include_router(conv_router, prefix="/conversation", tags=["Перетворення одиниць"])
+app.include_router(formulas.router, prefix="/formulas", tags=["Формули фігур на площині і в об'ємі"])
+app.include_router(facts_router, prefix="/facts", tags=["Рандомні математичні факти"])
+app.include_router(games_router, prefix="/games", tags=["Міні-ігри"])
